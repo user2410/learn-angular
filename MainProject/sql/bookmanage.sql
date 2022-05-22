a16f13fc1e6aa31c41e27804f8b57ff634b3f881
Hoang Anh <anh.lnh194471@sis.hust.edu.vn>
2022-05-22 09:53:11 +0700

    test data for db

--- MainProject/sql/bookmanage.sql
+++ MainProject/sql/bookmanage.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 20, 2022 at 11:24 AM
+-- Generation Time: May 22, 2022 at 04:50 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.1.32
 
@@ -32,9 +32,18 @@ CREATE TABLE `book` (
   `Title` varchar(50) DEFAULT NULL,
   `Author` varchar(300) DEFAULT NULL,
   `View_time` int(11) DEFAULT NULL,
+  `Like_time` int(11) DEFAULT NULL,
   `Img` blob DEFAULT NULL,
   `ImgType` enum('JPEG','PNG','GIF','TIFF','PSD','PDF','EPS','AI','INDD','RAW') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`BookID`, `Title`, `Author`, `View_time`, `Like_time`, `Img`, `ImgType`) VALUES
+(1, 'Book1', 'Author1, Author2, Author3', 10, 6, NULL, NULL),
+(2, 'Book2', 'Author2, Author4', 13, 7, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -47,6 +56,15 @@ CREATE TABLE `category` (
   `Name` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`ID_PL`, `Name`) VALUES
+(1, 'Sci-fi'),
+(2, 'Fantasy'),
+(3, 'Psychological');
+
 -- --------------------------------------------------------
 
 --
@@ -57,6 +75,15 @@ CREATE TABLE `phan_loai` (
   `ID_PL` int(11) NOT NULL,
   `BookID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `phan_loai`
+--
+
+INSERT INTO `phan_loai` (`ID_PL`, `BookID`) VALUES
+(3, 1),
+(1, 2),
+(2, 2);
 
 -- --------------------------------------------------------
 
